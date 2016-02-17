0d55d1cd0306ed23bd91d079ef1a9cb0151af8b4
programmer <523121837@qq.com>
2016-02-17 19:35:44 +0800

    卸载jade模块，确认使用expross+ejs

--- app.js
+++ app.js
@@ -1,37 +1,16 @@
 var express = require('express')
 var port = process.env.PORT || 3000
 var app = express()
-
+var title = '博客世界-blog-world';
 app.set('views','./views')
-app.set('view engine','jade')
-app.listen(port)
+app.set('view engine','ejs')
 
-console.log('运行成功！端口号是：'+port)
+app.listen(port)
 
 //index page
 app.get('/',function(req,res){
 	res.render('index',{
-		title:'博客世界-blog-world'
-	})
-})
-
-//index page
-app.get('/admin/list',function(req,res){
-	res.render('list',{
-		title:'文章列表-博客世界-blog-world'
-	})
-})
-
-//index page
-app.get('/movie/:id',function(req,res){
-	res.render('detail',{
-		title:'文章详情-博客世界-blog-world'
-	})
-})
-
-//index page
-app.get('/admin/movie',function(req,res){
-	res.render('admin',{
-		title:'添加文章-博客世界-blog-world'
+		title:title,
+		cont:'这里是ejs模板的首页'
 	})
 })
