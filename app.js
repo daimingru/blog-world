6a38bfd2db7750baadd7770abd821a6a03c99684
programme <523121837@qq.com>
2016-02-18 19:09:11 +0800

    ij

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 var express = require('express')
 var port = process.env.PORT || 3000
 var app = express()
-var title = '博客世界-blog-world';
+var title = '博客视界-blog-world';//博客标题title
+var rootpath = __dirname;//根目录
+express.static(path.join(__dirname, 'public'))
 app.set('views','./views')
-app.set('view engine','ejs')
+app.set('view engine','ejs')//ejs模板
 
 app.listen(port)
 
 //index page
 app.get('/',function(req,res){
+	indexaction(req,res);
+})
+
+app.get('/index',function(req,res){
+	indexaction(req,res);
+})
+
+function indexaction(req,res){
 	res.render('index',{
 		title:title,
-		cont:'这里是ejs模板的首页'
+		rootpath:rootpath
 	})
-})
+}
+
