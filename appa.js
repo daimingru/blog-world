87b1f8ebfe051983abb36ea107f52b33b97a6727
daimingru <daimingru@okay.cn>
2017-07-05 19:26:27 +0800

    测试微信

--- appa.js
+++ appa.js
@@ -45,7 +45,55 @@ app.get('/',function(req,res){
 });
 
 app.get('/index',function(req,res){
-    var return_uri = 'http%3A%2F%2Fwww.aparesse.com%2Findex'
+    var return_uri = 'http%3A%2F%2Fwww.aparesse.com%2Fget_wx'
     var url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxf114c69f6437bf60&redirect_uri='+return_uri+'&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
      res.redirect(url);
+});
+
+app.get('/get_wx', function(req,res, next){
+
+    // 第二步：通过code换取网页授权access_token
+    var code = req.query.code;
+    request.get(
+        {   
+            url:'https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxf114c69f6437bf60&secret='+AppSecret+'&code='+code+'&grant_type=authorization_code',
+        },
+        function(error, response, body){
+            if(response.statusCode == 200){
+
+                // 第三步：拉取用户信息(需scope为 snsapi_userinfo)
+                //console.log(JSON.parse(body));
+                var data = JSON.parse(body);
+                var access_token = data.access_token;
+                var openid = data.openid;
+
+                request.get(
+                    {
+                        url:'https://api.weixin.qq.com/sns/userinfo?access_token='+access_token+'&openid='+openid+'&lang=zh_CN',
+                    },
+                    function(error, response, body){
+                        if(response.statusCode == 200){
+
+                            // 第四步：根据获取的用户信息进行对应操作
+                            var userinfo = JSON.parse(body);
+                            //console.log(JSON.parse(body));
+                            console.log('获取微信信息成功！');
+
+                            // 小测试，实际应用中，可以由此创建一个帐户
+                            res.send("\
+                                <h1>"+userinfo.nickname+" 的个人信息</h1>\
+                                <p><img src='"+userinfo.headimgurl+"' /></p>\
+                                <p>"+userinfo.city+"，"+userinfo.province+"，"+userinfo.country+"</p>\
+                            ");
+
+                        }else{
+                            console.log(response.statusCode);
+                        }
+                    }
+                );
+            }else{
+                console.log(response.statusCode);
+            }
+        }
+    );
 });
