28447b8adbf86da16505a9f702658e425beb0b5f
daimingru <daimingru@okay.cn>
2017-07-05 18:39:20 +0800

    测试微信

--- appa.js
+++ appa.js
@@ -1,4 +1,9 @@
 var express = require('express')
 var port = process.env.PORT || 80
+var bodyParser = require("body-parser")
 var app = express()
 app.listen(port)
+app.use(bodyParser.urlencoded({ extended: false }))
+app.post('/',function(req,res){  
+  res.end(req.body);  
+});  
